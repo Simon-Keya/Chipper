ec75a32942780e34d03fb6e7904c5755dc6a6275
Simon Keya <keya8020@gmail.com>
2025-11-06 13:09:05 +0300

    commit

--- chipper/app/products/page.tsx
+++ chipper/app/products/page.tsx
@@ -1,6 +1,9 @@
-"use client";
+"use client"
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
+export const runtime = "nodejs"
 
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
